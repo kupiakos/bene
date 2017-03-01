f1225671131876e6bf4bea38b77888e0ca0bbc62
Kevin Haroldsen <kupiakos@gmail.com>
2017-03-01 04:28:22 -0700

    Remove Lab 1 material

--- lab1/reporthandler.py
+++
@@ -1,9 +0,0 @@
-from src.sim import Sim
-
-
-class ReportHandler:
-    @staticmethod
-    def receive_packet(packet):
-        print('%.3f' % float(packet.created), packet.ident, '%.3f' % Sim.scheduler.current_time())
-
-
