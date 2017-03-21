a28107b4456c5982e3d38fe13d3180a017f3906d
Kevin Haroldsen <kupiakos@gmail.com>
2017-03-21 01:30:57 -0600

    Refactor generate_graphs.sh

--- lab3/generate_graphs.sh
+++ lab3/generate_graphs.sh
@@ -8,16 +8,16 @@ plural() {
     echo "$1$([ $2 -eq 1 ] || echo s)"
 }
 
-transfer() {
+simulate() {
     mkdir tmp
-    ./transfer.py ${2:+-d} $2 internet-architecture.pdf tmp/sequence.csv tmp/cwnd.csv
+    ./transfer.py ${2:+-d} "$2" internet-architecture.pdf tmp/sequence.csv tmp/cwnd.csv
     ./tcp-plot.py tmp/sequence.csv tmp/cwnd.csv report/graphs/tahoe_${1} -t "Tahoe with $1 $(plural packet $1) dropped"
     rm -r tmp
 }
 
-transfer 0
-transfer 1 14000
-transfer 2 14000,28000
-transfer 3 14000,26000,28000
+simulate 0
+simulate 1 14000
+simulate 2 14000,28000
+simulate 3 14000,26000,28000
 
 popd
