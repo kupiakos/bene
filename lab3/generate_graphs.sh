9481686e9ce3409e604c04ea8615dbc45f7c7d2d
Kevin Haroldsen <kupiakos@gmail.com>
2017-03-21 04:47:00 -0600

    Update generate_graphs to support Reno

--- lab3/generate_graphs.sh
+++ lab3/generate_graphs.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname "$0")"
-rm -r report/graphs
-mkdir -p report/graphs
+pushd "$(dirname "$0")" >/dev/null
+rm -r report/graphs 2>/dev/null
+mkdir -p report/graphs 2>/dev/null
 
 plural() {
     echo "$1$([ $2 -eq 1 ] || echo s)"
 }
 
 simulate() {
-    mkdir tmp
-    ./transfer.py ${2:+-d} "$2" internet-architecture.pdf tmp/sequence.csv tmp/cwnd.csv
-    ./tcp-plot.py tmp/sequence.csv tmp/cwnd.csv report/graphs/tahoe_${1} -t "Tahoe with $1 $(plural packet $1) dropped"
+    mkdir tmp 2>/dev/null
+    for type in '' reno; do
+        ./transfer.py ${type:+-r} ${2:+-d $2} \
+            internet-architecture.pdf tmp/sequence.csv tmp/cwnd.csv || exit
+        ./tcp-plot.py tmp/sequence.csv tmp/cwnd.csv report/graphs/${type:=tahoe}-${1} \
+            -t "${type^} with $1 $(plural packet $1) dropped"
+    done
     rm -r tmp
 }
 
@@ -20,4 +24,5 @@ simulate 1 14000
 simulate 2 14000,28000
 simulate 3 14000,26000,28000
 
-popd
+popd > /dev/null
+echo 'Finished!'
